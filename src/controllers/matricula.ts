63adbe55725d4bfc4b8d035a6e88dd434c602b6d
Duvan Rosero <duvanrosero1995@gmail.com>
2021-04-05 10:48:54 -0500

    calcula precio matricula completa

--- src/controllers/matricula.ts
+++ src/controllers/matricula.ts
@@ -1,35 +1,34 @@
 import { getInfoMatricula, getDetPeriodo } from "../provider/matricula_provider";
 import { getConfigPeriodo, getPaquete, getDescuento } from "../provider/pago_provider";
-import { parse,format  } from 'date-format-parse';
+import { parse, format } from 'date-format-parse';
 
 //====================
 //   /matricula/generarpagomatricula 
 //=====================
 export const generarpagoMatricula = async (req: any, res: any) => {
-    let fechaActual = format(new Date(),  'YYYY-MM-DD');
+    let fechaActual = format(new Date(), 'YYYY-MM-DD');
     let token = req.body.token;
-    let resultDB:any;
-    let resultPaquete:any;
-    let total =0;
-    let total_a_pagar=0;
-    let total_con_descuento =0;
+    let resultDB: any;
+    let resultPaquete: any;
+    let total = 0;
+    let total_a_pagar = 0;
+    let total_con_descuento = 0;
     let total_sin_descuento = 0;
     let porcentaje_descuento = 0;
-    let precios : any;
-    let periodo:any;
+    let precios: any;
+    let periodo: any;
     let id_matricula = req.params.id_matricula.trim();
     try {
         let result = await getInfoMatricula(id_matricula);
         if (result[0].length > 0) {
             resultDB = result[0][0];
- 
             let resultConfig = await getConfigPeriodo();
             //para consultar las fechas de matriculas 
-            periodo = await getDetPeriodo(resultDB.cod_colegio,resultDB.cod_periodo,fechaActual);
+            periodo = await getDetPeriodo(resultDB.cod_colegio, resultDB.cod_periodo, fechaActual);
 
             //consular los descuentos que un estudiante tiene asignados
             let resultDto = await getDescuento(resultDB.cod_matricula, resultDB.cod_periodo);
-            resultDto.forEach((row:any) => {
+            resultDto.forEach((row: any) => {
                 porcentaje_descuento = porcentaje_descuento + row.porcentaje;
             });
 
@@ -37,81 +36,81 @@ export const generarpagoMatricula = async (req: any, res: any) => {
             //configurar deacuerdo a la configuracion del periodo
             if (resultDB.nro_creditos <= resultConfig.min_creditos) {
 
-                //TODO: verificar de que clico es para calcular el precio
+                //se debe cobrar por credito individual
+                console.log("Se cobra por creditos");
 
-                           //ciclo tecnologico
-                if(resultDB.cod_nivel_edu==6){
+                //ciclo tecnologico
+                if (resultDB.cod_nivel_edu == 6) {
                     resultPaquete = await getPaquete(resultDB.cod_periodo, 1);
-                }else if(resultDB.cod_nivel_edu==7){
+                } else if (resultDB.cod_nivel_edu == 7) {
                     resultPaquete = await getPaquete(resultDB.cod_periodo, 4);
                 }
 
-                //se debe cobrar por credito individual
-                console.log("Se cobra por creditos");
-                resultPaquete = await getPaquete(resultDB.cod_periodo, 1);
+
+                // resultPaquete = await getPaquete(resultDB.cod_periodo, 1);
 
 
                 if (resultPaquete != false) {
 
                     precios = resultPaquete;
                     //recorrer los detalles de paquete
-                    precios.forEach((element:any, index:number) => {
-                        
+                    precios.forEach((element: any, index: number) => {
+
 
                         //si se puede aplicar descuento externo
-                        if(element.descuento_ext=='1'){
-                            
-                            if(periodo==false){
+                        if (element.descuento_ext == '1') {
+
+                            if (periodo == false) {
                                 precios[index].aumento = resultConfig.porcentaje_ext;
                             }
-                            
 
-                            let totaAPagar= 0;
-                            if(element.cantidad > 0){
+
+                            let totaAPagar = 0;
+                            if (element.cantidad > 0) {
                                 totaAPagar = totaAPagar + (element.subtotal * element.cantidad);
-                            }else{
+                            } else {
                                 precios[index].cantidad = resultDB.nro_creditos;
                                 precios[index].descuento = porcentaje_descuento;
-                                porcentaje_descuento=0;
+                                porcentaje_descuento = 0;
                                 total = element.subtotal * Number(resultDB.nro_creditos);
-                                totaAPagar = totaAPagar +  total ;
+                                totaAPagar = totaAPagar + total;
                             }
                             total_con_descuento = totaAPagar - (totaAPagar * porcentaje_descuento);
 
-                        }else{
-                            let totaAPagar= 0;
-                            if(element.cantidad > 0){
+                        } else {
+                            let totaAPagar = 0;
+                            if (element.cantidad > 0) {
                                 totaAPagar = totaAPagar + (element.subtotal * element.cantidad);
-                            }else{
+                            } else {
                                 total = element.subtotal * Number(resultDB.nro_creditos);
-                                totaAPagar = totaAPagar +  total ;
+                                totaAPagar = totaAPagar + total;
                                 precios[index].cantidad = resultDB.nro_creditos;
                             }
                             total_sin_descuento = totaAPagar;
                         }
 
                         //calcula el total sin descuento
-                        if(element.cantidad > 0){
+                        if (element.cantidad > 0) {
                             total_a_pagar = total_a_pagar + (element.subtotal * element.cantidad);
-                        }else{
+                        } else {
                             total = element.subtotal * Number(resultDB.nro_creditos);
-                            total_a_pagar = total_a_pagar +  total ;
+                            total_a_pagar = total_a_pagar + total;
                         }
                         total_sin_descuento = total_a_pagar;
-                        
+
                     });
 
 
                     //volvemos a recorrer para calcular totales
                     total_a_pagar = 0;
-                    precios.forEach((element:any, index:number) => {
+                    precios.forEach((element: any, index: number) => {
                         let subtotal = (element.valor_unidad * element.cantidad);
-                        precios[index].subtotal = ( subtotal +  ( subtotal * element.aumento) ) -  ( subtotal * element.descuento)
+                        precios[index].subtotal = (subtotal + (subtotal * element.aumento)) - (subtotal * element.descuento)
                         total_a_pagar = element.subtotal + total_a_pagar;
                     });
 
 
-                    
+
 
 
                 } else {
@@ -126,32 +125,80 @@ export const generarpagoMatricula = async (req: any, res: any) => {
                 console.log("Se cobra matricula completa");
 
                 //ciclo tecnologico
-                if(resultDB.cod_nivel_edu==6){
+                if (resultDB.cod_nivel_edu == 6) {
                     resultPaquete = await getPaquete(resultDB.cod_periodo, 2);
-                }else if(resultDB.cod_nivel_edu==7){
+                } else if (resultDB.cod_nivel_edu == 7) {
                     resultPaquete = await getPaquete(resultDB.cod_periodo, 3);
                 }
 
-               
                 if (resultPaquete != false) {
 
 
+
+                    precios = resultPaquete;
+                    //recorrer los detalles de paquete
+                    precios.forEach((element: any, index: number) => {
+
+
+                        //si se puede aplicar descuento externo
+                        if (element.descuento_ext == '1') {
+
+                            //añade aumento para matricula extraordinaria
+                            if (periodo == false) {
+                                precios[index].aumento = resultConfig.porcentaje_ext;
+                            }
+
+                            let totaAPagar = 0;
+                            totaAPagar = totaAPagar + (element.subtotal * element.cantidad);
+                            precios[index].descuento = porcentaje_descuento;
+
+
+                            total_con_descuento = totaAPagar - (totaAPagar * porcentaje_descuento);
+
+                        } else {
+                            let totaAPagar = 0;
+                            totaAPagar = totaAPagar + (element.subtotal * element.cantidad);
+                            total_sin_descuento = totaAPagar;
+                        }
+
+                        //calcula el total sin descuento
+                        if (element.cantidad > 0) {
+                            total_a_pagar = total_a_pagar + (element.subtotal * element.cantidad);
+                        } else {
+                            total = element.subtotal * Number(resultDB.nro_creditos);
+                            total_a_pagar = total_a_pagar + total;
+                        }
+                        total_sin_descuento = total_a_pagar;
+
+                    });
+
+                    //volvemos a recorrer para calcular totales
+                    total_a_pagar = 0;
+                    precios.forEach((element: any, index: number) => {
+                        let subtotal = (element.valor_unidad * element.cantidad);
+                        precios[index].subtotal = (subtotal + (subtotal * element.aumento)) - (subtotal * element.descuento)
+                        total_a_pagar = element.subtotal + total_a_pagar;
+                    });
+
+                    
                 } else {
                     throw new Error("No se encontraron precios configurados");
                 }
 
 
-                
+
 
             }
 
 
             return res.json({
                 error: false,
-                message: "Funcionando matricula",
-                detalle_factura : resultPaquete,
+                message: "Ejecución correcta",
+                matricula: resultDB,
+                detalle_factura: resultPaquete,
                 total_a_pagar,
                 total_sin_descuento
+
 
             });
 
