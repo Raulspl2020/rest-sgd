1ab11f3f9c93a2324cc44944fcb3dfae00ce4ed0
Duvan Rosero <duvanrosero1995@gmail.com>
2023-07-27 13:03:18 -0500

    refactor: change value eviroment default

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 //=========================
 //   ENTORNO
 //=========================
-process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //=========================
 //   PUERTO SERVER
