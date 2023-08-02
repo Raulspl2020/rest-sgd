e8086c6b8b84eefd757eeb43209274c77003cf88
Duvan Rosero <drosero@talenta.cloud>
2023-08-01 20:25:17 -0500

    refactor: configure database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,59 +1,26 @@
 import Knex from 'knex';
 import { connect } from "mssql";
 import mongoose from 'mongoose';
-const db: any = {
-  desarrollo: {
-    host: process.env.MYSQL_DEV_SGD_HOST,
-    user: process.env.MYSQL_DEV_SGD_USER,
-    password: process.env.MYSQL_DEV_SGD_PASS,
-    database: process.env.MYSQL_DEV_SGD_DATABASE,
+
+
+const db = {
+  sigedin: {
+    host: process.env.MYSQL_SGD_HOST,
+    user: process.env.MYSQL_SGD_USER,
+    password: process.env.MYSQL_SGD_PASS,
+    database: process.env.MYSQL_SGD_DATABASE,
     port: 3306,
   },
 
-  produccion: {
-    host: process.env.MYSQL_PROD_SGD_HOST,
-    user: process.env.MYSQL_PROD_SGD_USER,
-    password: process.env.MYSQL_PROD_SGD_PASS,
-    database: process.env.MYSQL_PROD_SGD_DATABASE,
-    port: 3306,
-  },
-};
-
-const auth: any = {
-  desarrollo: {
-    host: process.env.MYSQL_DEV_AUTH_HOST,
-    user: process.env.MYSQL_DEV_AUTH_USER,
-    password: process.env.MYSQL_DEV_AUTH_PASS,
-    database: process.env.MYSQL_DEV_AUTH_DATABASE,
-    port: 3306,
-  },
-
-  produccion: {
-    host: process.env.MYSQL_PROD_AUTH_HOST,
-    user: process.env.MYSQL_PROD_AUTH_USER,
-    password: process.env.MYSQL_PROD_AUTH_PASS,
-    database: process.env.MYSQL_PROD_AUTH_DATABASE,
+  seguridad: {
+    host: process.env.MYSQL_SGD_HOST,
+    user: process.env.MYSQL_SGD_USER,
+    password: process.env.MYSQL_SGD_PASS,
+    database: process.env.MYSQL_SGD_DATABASE,
     port: 3306,
   },
 };
 
-if (process.env.NODE_ENV == "pro") {
-  db.default = db.produccion;
-  auth.default = auth.produccion;
-} else {
-  db.default = db.desarrollo;
-  auth.default = auth.desarrollo;
-}
-
-
-let conDB: any = Knex({
-  client: "mysql",
-  connection: db.default,
-});
-let conAuth = Knex({
-  client: "mysql",
-  connection: auth.default,
-});
 
 const sqlConfig = {
   user: process.env.MSSQL_DEV_USER,
@@ -72,7 +39,20 @@ const sqlConfig = {
 }
 
 
-//establece una coneccion con sqlserver
+
+let conDB: any = Knex({
+  client: "mysql",
+  connection: db.sigedin,
+});
+let conAuth = Knex({
+  client: "mysql",
+  connection: db.seguridad,
+});
+
+
+
+
+//establece una coneccion con sqlserver para sysApolo
 const conSysApolo = async () => {
   try {
     let conexion = await connect(sqlConfig)
@@ -83,20 +63,18 @@ const conSysApolo = async () => {
   }
 }
 
-const mongo_cnn_url = ():string => {
+const mongo_cnn_url = (): string => {
   return `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_DB_PORT}/${process.env.MONGO_DB_NAME}`
 }
 
 //conectar a base de datos mongo
 const conMongo = async () => {
-
   try {
-    console.log("starting connection",mongo_cnn_url())
+    console.log("starting connection", mongo_cnn_url())
     await mongoose.connect(mongo_cnn_url());
     console.log("base de datos MONGO online");
   } catch (error) {
     throw new Error("Error de conexion: " + error);
-
   }
 
 }
